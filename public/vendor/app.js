83f90ecca9763ac73e9672d4cfae5c59cc0de252
dmitry <shudv@yandex.ru>
2022-06-17 12:58:26 +0300

    add method get_orger to js

--- public/vendor/app.js
+++ public/vendor/app.js
@@ -23,8 +23,19 @@ function reduce_from_basket(id){
 }
 
   
-function show_counter(id,x) {
-  
+function show_counter(id,x) { 
   var el = document.getElementById(id);
   el.innerHTML = x;  
+}
+
+function get_order() {
+  var order='';
+  for(i=0; i < window.localStorage.length; i++){
+    var key = window.localStorage.key(i);
+    var value = window.localStorage.getItem(key);
+    if(key.indexOf('product_')==0){
+      order = order + key + "=" + value +", "; 
+    }
+  }
+return order;
 }
