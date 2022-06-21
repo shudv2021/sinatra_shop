4d1df41eadcdd333a2c8efb8e973ead72d1d6714
dmitry <shudv@yandex.ru>
2022-06-21 14:37:13 +0300

    create page order, get prder parsms

--- app.rb
+++ app.rb
@@ -3,10 +3,14 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'pry'
 
 set :database, {adapter: "sqlite3", database: "shop_ar.db"}
 
 class Product < ActiveRecord::Base
+end
+
+class Order < ActiveRecord::Base
 end
 
 get '/' do
@@ -18,6 +22,9 @@ get '/goods' do
   erb :goods
 end
 
-post '/parchase' do
-
+post '/orders' do
+  @order = params[:order]
+  @order.chomp!(', ').gsub!('product_')
+    
+  erb :orders
 end
